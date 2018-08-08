eab75161fd43a0bed4830570a1e33ca9d8a705a2
Daniel Rondón <danfelrondon@gmail.com>
2018-08-08 01:36:05 -0400

    FireBase Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyClh8FBSIg1TbC1hA8q9J620o1dPR8O9Rs",
+    authDomain: "myloginapp-78c3d.firebaseapp.com",
+    databaseURL: "https://myloginapp-78c3d.firebaseio.com",
+    projectId: "myloginapp-78c3d",
+    storageBucket: "myloginapp-78c3d.appspot.com",
+    messagingSenderId: "189038002410"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyClh8FBSIg1TbC1hA8q9J620o1dPR8O9Rs",
+    authDomain: "myloginapp-78c3d.firebaseapp.com",
+    databaseURL: "https://myloginapp-78c3d.firebaseio.com",
+    projectId: "myloginapp-78c3d",
+    storageBucket: "myloginapp-78c3d.appspot.com",
+    messagingSenderId: "189038002410"
+  }
 };
 
 /*
